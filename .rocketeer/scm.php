68f9ca83d5e4fa53bea0fc0413e091fec7615986
julian ladjani <julian.ladjani@epitech.eu>
2020-09-19 16:49:18 +0200

    [Deply] Deploy

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -10,7 +10,7 @@ return [
 
     // The SSH/HTTPS address to your repository
     // Example: https://github.com/vendor/website.git
-    'repository' => 'gitlab.com:diabeteam/showcase-website.git',
+    'repository' => 'git@github.com:Diabetips/diabetips-website.git',
 
     // The repository credentials : you can leave those empty
     // if you're using SSH or if your repository is public
